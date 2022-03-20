d2cb9ddf2e8b60e0ffdb170c3f2e0ce7ffa6358b
Lucien98 <2172856294@qq.com>
2022-03-20 17:30:45 +0800

    DPA using iterated style of matrix operation

--- CPAv2.cs
+++ CPAv2.cs
@@ -22,33 +22,18 @@ public class CPAv2
     private BitArray Round1Key = new BitArray(48);
     Vector<Double> ResA2ColumnSumVec;
     Vector<Double> ResB2ColumnSumVec;
-    Vector<Double> ResAaverageVec;
-    Vector<Double> ResBaverageVec;
     Vector<Double> ResAColumnSumsVec;
     Vector<Double> ResBColumnSumsVec;
-    Vector<Double> ResAvarianceVec;
-    Vector<Double> ResBvarianceVec;
+    Vector<Double> ResOneMinusAColumnSumsVec;
     Matrix<Double> X;
+    Matrix<Double> W;
     public CPAv2(String fileName)
     {
         trsWorker = new TrsWorker(fileName);
         int NT = trsWorker.getTracesNumber();
         hypothesis = Matrix<double>.Build.Dense(NT, 8 * blockSize);
-        
-        int NS = trsWorker.getSampleNumber();
-        ResA2ColumnSumVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResB2ColumnSumVec = Vector<Double>.Build.Dense(NS);
-        
-        ResAaverageVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResBaverageVec = Vector<Double>.Build.Dense(NS);
-        ResAColumnSumsVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResBColumnSumsVec = Vector<Double>.Build.Dense(NS);
-
-        ResAvarianceVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResBvarianceVec = Vector<Double>.Build.Dense(NS);
-        X = Matrix<Double>.Build.Dense(8*blockSize, NS);
+        initMatrix();
     }
-
 
     private void initMatrix()
     {
@@ -56,15 +41,12 @@ public class CPAv2
 
         ResA2ColumnSumVec = Vector<Double>.Build.Dense(8 * blockSize);
         ResB2ColumnSumVec = Vector<Double>.Build.Dense(NS);
-
-        ResAaverageVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResBaverageVec = Vector<Double>.Build.Dense(NS);
         ResAColumnSumsVec = Vector<Double>.Build.Dense(8 * blockSize);
+        ResOneMinusAColumnSumsVec = Vector<Double>.Build.Dense(8 * blockSize);
         ResBColumnSumsVec = Vector<Double>.Build.Dense(NS);
 
-        ResAvarianceVec = Vector<Double>.Build.Dense(8 * blockSize);
-        ResBvarianceVec = Vector<Double>.Build.Dense(NS);
         X = Matrix<Double>.Build.Dense(8 * blockSize, NS);
+        W = Matrix<Double>.Build.Dense(8 * blockSize, NS);
 
     }
     private int bitCount(int i)
@@ -140,13 +122,12 @@ public class CPAv2
         }
         else
         {
-            //need to change with start and N staffs
             Matrix<Double> hypothesis1 = Matrix<double>.Build.Dense(N, 8 * blockSize);
             if (round == 1) plainTextsList = trsWorker.extractPlainTextsList(start, N);
             else if (round == 2) computeDES1stRoundResult();
             byte[] LRIndices = { DES.DES.getLRIndices(sboxNumber)[0] };
 
-            for (int i = start; i < plainTextsList.Count && i < start + N; i++)
+            for (int i = 0; i < plainTextsList.Count; i++)
             {
 
                 var IPL = DES.DES.getIPL(Utils.HexStringToByteArray(plainTextsList[i]), round);
@@ -154,8 +135,9 @@ public class CPAv2
 
 
                 var extended = DES.DES.ETrans(IPR);
-                var IPLCharacBits = DES.DES.extract(IPL, DES.DES.getLRIndices(sboxNumber));
-                var IPRCharacBIts = DES.DES.extract(IPR, DES.DES.getLRIndices(sboxNumber));
+                var IPLCharacBits = DES.DES.extract(IPL, LRIndices);
+                var IPRCharacBIts = DES.DES.extract(IPR, LRIndices);
+
 
                 for (int j = 0; j < 64; j++)
                 {
@@ -163,7 +145,8 @@ public class CPAv2
                     int sboxInputInt = bitArrayToInt(sboxInput);
                     sboxInputInt = sboxInputInt ^ j;
                     var sboxOutput = DES.DES.getSBoxContent(sboxNumber, sboxInputInt);
-                    hypothesis1[i-start, j] = bitCount(sboxOutput ^ bitArrayToInt(IPLCharacBits) ^ bitArrayToInt(IPRCharacBIts));
+                    sboxOutput = sboxOutput >> 3;
+                    hypothesis1[i, j] = bitCount(sboxOutput ^ bitArrayToInt(IPLCharacBits) ^ bitArrayToInt(IPRCharacBIts));
                 }
             }
             return hypothesis1;
@@ -207,28 +190,30 @@ public class CPAv2
 
         key = new byte[blockSize];
         
+        if (method == "CPA")
         for (int N = 1; N < blockSize + 1; N++)
         {
             initMatrix();
             attackTracesNumber = 0;
             for (int round = 2; round <= 2; round++)
             {
+                    int step = 20000;
                 for (int i = 0; i < 1; i++)
                 {
                     Console.WriteLine(i);
                     beforeDT = DateTime.Now;
-                    initDesHypothesis(N, 1, method, 10000 * i, 10000);
-                    var hypothesis2 = initDesHypothesis(N, round, method, 10000 * i, 10000);
+                    initDesHypothesis(N, 1, method, step * i, step);
+                    var hypothesis2 = initDesHypothesis(N, round, method, step * i, step);
                     afterDT = System.DateTime.Now;
                     ts = afterDT.Subtract(beforeDT);
                     Console.WriteLine("init hypothesis done! Elsped {0} s", ts);
 
                     beforeDT = DateTime.Now;
                 
-                        dataTraces = trsWorker.extractNTracesMatrix(10000*i, 10000);
-                        correlate2(hypothesis2);
+                        dataTraces = trsWorker.extractNTracesMatrix(step * i, step);
+                    correlate2(hypothesis2);
+                    //correlationMatrix = differential(hypothesis2, dataTraces);
 
-                
                     afterDT = System.DateTime.Now;
                     ts = afterDT.Subtract(beforeDT);
                     Console.WriteLine("init correlation matrix done! Elapsed {0} s", ts);
@@ -246,6 +231,48 @@ public class CPAv2
                 }
             }
         }
+        if (method == "DPA")
+        {
+            for (int N = 1; N < blockSize + 1; N++)
+            {
+                initMatrix();
+                attackTracesNumber = 0;
+                for (int round = 1; round <= 1; round++)
+                {
+                    int step = 20000;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(i);
+                        beforeDT = DateTime.Now;
+                        var hypothesis2 = initDesHypothesis(N, round, method, step * i, step);
+                        afterDT = System.DateTime.Now;
+                        ts = afterDT.Subtract(beforeDT);
+                        Console.WriteLine("init hypothesis done! Elsped {0} s", ts);
+
+                        beforeDT = DateTime.Now;
+
+                        dataTraces = trsWorker.extractNTracesMatrix(step * i, step);
+                        correlationMatrix = differential(hypothesis2, dataTraces);
+
+                        afterDT = System.DateTime.Now;
+                        ts = afterDT.Subtract(beforeDT);
+                        Console.WriteLine("init correlation matrix done! Elapsed {0} s", ts);
+                    }
+                    Vector<Double> absMaxOfEveryRow = Vector<Double>.Build.Dense(hypothesis.ColumnCount);
+                    for (int i = 0; i < absMaxOfEveryRow.Count; i++)
+                    {
+                        absMaxOfEveryRow[i] = correlationMatrix[i, correlationMatrix.Row(i).AbsoluteMaximumIndex()];
+                    }
+                    key[N - 1] = Convert.ToByte(absMaxOfEveryRow.AbsoluteMaximumIndex());
+                    Console.WriteLine("{0:X2}", key[N - 1]);
+                    for (var i = 0; i < 6; i++)
+                    {
+                        Round1Key[6 * (8 - N) + i] = Convert.ToBoolean(key[N - 1] >> i & 1);
+                    }
+                }
+            }
+
+        }
         DES.DES.printBitArray(Round1Key);
         trsWorker.died();
     }
@@ -257,33 +284,50 @@ public class CPAv2
         
     }
 
+    private Matrix<Double> differential(Matrix<Double> A, Matrix<Double> B)
+    {
+        Vector<Double> AColumnSumsVec = A.ColumnSums();
+        Vector<Double> OneMinusAColumnSumsVec = (1-A).ColumnSums();
+
+        ResAColumnSumsVec += AColumnSumsVec;
+        ResOneMinusAColumnSumsVec += OneMinusAColumnSumsVec;
+
+        Matrix<Double> ResAColumnSumsMatrix = Matrix<Double>.Build.Dense(A.ColumnCount, B.ColumnCount);
+        Matrix<Double> ResOneMinusAColumnSumsMatrix = Matrix<Double>.Build.Dense(A.ColumnCount, B.ColumnCount);
+
+        for(int i = 0; i < B.ColumnCount; i++)
+        {
+            ResAColumnSumsMatrix.SetColumn(i, ResAColumnSumsVec);
+            ResOneMinusAColumnSumsMatrix.SetColumn(i, ResOneMinusAColumnSumsVec);
+        }
+        X += A.Transpose() * B;
+        W += (1 - A).Transpose() * B;
+        return X.PointwiseDivide(ResAColumnSumsMatrix) - W.PointwiseDivide(ResOneMinusAColumnSumsMatrix); 
+
+
+    }
+
     private Matrix<Double> correlate(Matrix<Double> A, Matrix<Double> B)
     {
         attackTracesNumber += A.RowCount;
-        //Console.WriteLine(attackTracesNumber);
         Vector<Double> A2ColumnSumVec = A.PointwisePower(2).ColumnSums();
         Vector<Double> B2ColumnSumVec = B.PointwisePower(2).ColumnSums();
 
         ResA2ColumnSumVec += A2ColumnSumVec;
         ResB2ColumnSumVec += B2ColumnSumVec;
 
-        Vector<Double> AaverageVec = A.ColumnSums() / A.RowCount;
-        Vector<Double> BaverageVec = B.ColumnSums() / B.RowCount;
         Vector<Double> AColumnSumsVec = A.ColumnSums();
         Vector<Double> BColumnSumsVec = B.ColumnSums();
 
         ResAColumnSumsVec += AColumnSumsVec;
         ResBColumnSumsVec += BColumnSumsVec;
 
-        //Vector<Double> AvarianceVec = A2ColumnSumVec - A.RowCount * (AaverageVec.PointwisePower(2));
-        //Vector<Double> BvarianceVec = B2ColumnSumVec - B.RowCount * (BaverageVec.PointwisePower(2));
         Vector<Double> AvarianceVec = ResA2ColumnSumVec - (AColumnSumsVec.PointwisePower(2)) / attackTracesNumber;
         Vector<Double> BvarianceVec = ResB2ColumnSumVec - (BColumnSumsVec.PointwisePower(2)) / attackTracesNumber;
 
 
 
         X += A.Transpose()*B;
-        //Matrix<Double> Y = A.RowCount*(AaverageVec.ToColumnMatrix()*BaverageVec.ToRowMatrix());
         Matrix<Double> Y = (ResAColumnSumsVec.ToColumnMatrix() * ResBColumnSumsVec.ToRowMatrix()) / attackTracesNumber;
 
         Matrix<Double> Z = AvarianceVec.ToColumnMatrix() * BvarianceVec.ToRowMatrix();
